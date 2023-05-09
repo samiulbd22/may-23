763a8e4f367ba83e4784eff595a0be2656f9ac35
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2023-05-09 12:36:28 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -466,9 +466,12 @@ function previousListShowMe() {
 }
 
 const meal_get_money = document.getElementsByClassName("meal_get_money");
-const getContent = meal_get_money.innerText;
-if(getContent === 0){
- meal_get_money.style.color = "red";
+;
+for(let i = 0; i < meal_get_money.length; i++ ){
+    let getContent = meal_get_money[i].innerText;
+    if( getContent === "0"){
+        getContent.style.color = "red";
+    }
 }
 
 
